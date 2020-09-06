c084df098eda1416989644cd24aacb2370f9f640
Joanna Brzuchacz <joannabrzuchacz@gmail.com>
2020-09-06 14:14:46 +0200

    Implement tests and function passing all tests

--- src/main/java/com/codecool/FizzBuzz.java
+++ src/main/java/com/codecool/FizzBuzz.java
@@ -7,8 +7,16 @@ public class FizzBuzz {
 
     public List<String> convertToList(int i) {
         List<String> stringList = new LinkedList<>();
-        for(int j = 1; j <= i; j++){
-            stringList.add(String.valueOf(j));
+        for (int j = 1; j <= i; j++) {
+            if (j % 3 == 0 && j % 5 == 0) {
+                stringList.add("FizzBuzz");
+            } else if (j % 3 == 0) {
+                stringList.add("Fizz");
+            } else if (j % 5 == 0) {
+                stringList.add("Buzz");
+            } else {
+                stringList.add(String.valueOf(j));
+            }
         }
         return stringList;
     }
--- src/test/java/com/codecool/FizzBuzzTest.java
+++ src/test/java/com/codecool/FizzBuzzTest.java
@@ -1,9 +1,53 @@
 package com.codecool;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
+    List<String> stringList;
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setup() {
+        stringList = new LinkedList<>();
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void shouldConvert1To1String() {
+        stringList = fizzBuzz.convertToList(1);
+        assertEquals(Arrays.asList("1"), stringList);
+    }
+
+    @Test
+    public void shouldConvert2ToListOfString() {
+        stringList = fizzBuzz.convertToList(2);
+        assertEquals(Arrays.asList("1", "2"), stringList);
+    }
+
+    @Test
+    public void shouldCovert3ToListOfString() {
+        stringList = fizzBuzz.convertToList(3);
+        assertEquals(Arrays.asList("1", "2", "Fizz"), stringList);
+    }
+
+    @Test
+    public void shouldCovert5ToListOfString() {
+        stringList = fizzBuzz.convertToList(5);
+        assertEquals(Arrays.asList("1", "2", "Fizz", "4", "Buzz"), stringList);
+    }
+
+    @Test
+    public void shouldCovert15ToListOfString() {
+        stringList = fizzBuzz.convertToList(15);
+        assertEquals(Arrays.asList("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+                "11", "Fizz", "13", "14", "FizzBuzz"), stringList);
+    }
 }
